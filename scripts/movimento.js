dbba3a2b228b114380c650495f33f5e5d85d4c24
avmoonlight <nalivssx@gmail.com>
2025-05-23 15:46:26 -0300

    arrumando

---
+++ scripts/movimento.js
@@ -0,0 +1,11 @@
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  });
+
+  document.querySelectorAll('.animate-on-scroll').forEach(el => {
+    observer.observe(el);
+  });
